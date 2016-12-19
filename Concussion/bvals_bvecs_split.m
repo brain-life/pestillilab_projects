eb0cfb11f19e8bf6dfa41160e36110399b7aaa7f
Bradley Caron <bacaron@hm012.karst.uits.iu.edu>
2016-12-19 12:14:51 -0500

    I added files that combines phase directions and creates raw data file, processes the t1 anatomical, splits the shells, runs dti_init, mrtrix ensemble, and segments fibers

--- Concussion/bvals_bvecs_split.m
+++ Concussion/bvals_bvecs_split.m
@@ -1,12 +1,14 @@
 function bvals_bvecs_split
+
 % seperate bvals/bvecs
 % build paths
-subj = { '1_5' };
+subj = '1_5';
 b_vals = {'1000','2000'};
 stem = 'data';
-projdir1 = 'pwd';
+projdir1 = '/N/dc2/projects/lifebid/Concussion/concussion_test';
 
 % Split data into two separate files (BVALS = 1000 and 2000).
+
 bvals = dlmread('data.bvals');
 bvals = round(bvals./100)*100;
 bvals(bvals==100) = 0;
@@ -19,7 +21,9 @@ dwi2000.fname = 'data_b2000.nii.gz';
 index1000 = (bvals == 1000);
 index2000 = (bvals == 2000);
 index0    = (bvals == 0);
+
 % Find all indices to each bvalue and B0
+
 all_1000  = or(index1000,index0);
 all_2000  = or(index2000,index0);
 dwi1000.data = dwi.data(:,:,:,all_1000);
@@ -38,9 +42,11 @@ dlmwrite('data_b2000.bvals',bvals2000);
 bvecs2000 = dlmread('data.bvecs');
 bvecs2000 = bvecs2000(:,all_2000);
 dlmwrite('data_b2000.bvecs',bvecs2000);
+
 for ibv = 1:length(b_vals)
-bvecs = fullfile(projdir1, sprintf('data_b%s.bvecs',b_vals{ibv}));
-bvals = fullfile(projdir1, sprintf('data_b%s.bvals',b_vals{ibv}));
-out   = fullfile(projdir1, 'fibers', sprintf('data_b%s.b', b_vals{ibv}));
+bvecs = fullfile(projdir1, subj, 'diffusion_data', sprintf('data_b%s.bvecs',b_vals{ibv}));
+bvals = fullfile(projdir1, subj, 'diffusion_data', sprintf('data_b%s.bvals',b_vals{ibv}));
+out   = fullfile(projdir1, subj, 'diffusion_data', 'fibers', sprintf('data_b%s.b', b_vals{ibv}));
 mrtrix_bfileFromBvecs(bvecs, bvals, out);
 end
+exit;
